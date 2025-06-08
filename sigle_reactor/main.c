fa55245f214866dcb7d7e76b2fa9358df013a16c
eric <eric@git.com>
2025-06-08 20:26:18 +0800

    third commit

---
+++ sigle_reactor/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "server.h"
+#include "unistd.h"
+
+int main(int argc, char const *argv[])
+{
+    if(argc < 3){
+        printf("./a.out port path\n");
+        return -1;
+    }
+    unsigned short port = atoi(argv[1]);
+    //切换服务器的工作路径
+    chdir(argv[2]);
+    //初始化用于监听的套接字
+    int lfd = initListenFd(port);
+    //启动服务器程序
+    epollRun(lfd);
+    return 0;
+}
--- sigle_reactor/server.h
+++ sigle_reactor/server.h
@@ -9,6 +9,22 @@ int epollRun(int lfd);
 //和客户端建立连接
 int acceptClient(int lfd,int epfd);
 
-
 // 主要是接收对端的数据
 int recvHttpRequest(int cfd,int epfd);
+
+// 解析请求行
+int parseRequestLine(const char* line,int cfd);
+
+// 发送响应头(状态行 + 响应头)
+int sendHeadMsg(int cfd,int status,const char* descr,const char* type,int length);
+const char* getFileType(const char* name);
+
+// 发送文件
+int sendFile(const char* fileName,int cfd);
+
+// 发送目录
+int sendDir(const char* dirName,int cfd); 
+
+// 解决浏览器无法访问带特殊字符的文件得到问题
+int hexToDec(char c);
+void decodeMsg(char* to,char* from);
